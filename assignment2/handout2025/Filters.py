52f12342e26eefb4df972726dcef848cc4854772
Magnus Gäfvert <magnus.gafvert@gmail.com>
2025-02-23 01:21:11 +0100

    Implemented smoother. Not yet correct. Assuming problem with consistent indexing. To check: - HMMSmoother loop - stored result arrays

--- assignment2/handout2025/Filters.py
+++ assignment2/handout2025/Filters.py
@@ -8,6 +8,10 @@ from models import *
 #
 # Add your Filtering / Smoothing approach(es) here
 #
+
+def normalize(f):
+    return f/np.sum(f)
+
 class HMMFilter:
     def __init__(self, probs, tm, om, sm):
         # if d=0 then forward filtering else smoothing with lag d 
@@ -16,21 +20,19 @@ class HMMFilter:
         self.__sm = sm
         self.__f = probs # appears to be uniform (where is this set?)
         # print(probs)
-     
+      
  
     # sensorR is the sensor reading (index!), self._f is the probability distribution resulting from the filtering                  
     def filter(self, reading) :
         O = self.__om.get_o_reading(reading) # if failure (None)?
         T = self.__tm.get_T()
         # Updates self.__f per forward filter (AIMA: forward 14.12)
-        self.__f = O@self.__tm.get_T().T@self.__f
-        alpha = 1/np.sum(self.__f)
-        self.__f = self.__f*alpha 
-
+        self.__f = normalize(O@self.__tm.get_T().T@self.__f)
         return self.__f
 
 class HMMSmoother:
-    
+    # https://en.wikipedia.org/wiki/Forward%E2%80%93backward_algorithm
+
     def __init__(self, tm, om, sm):
         self.__tm = tm
         self.__om = om
@@ -40,7 +42,13 @@ class HMMSmoother:
     # f_k is the filtered result (f_vector) for step k
     # fb is the smoothed result (fb_vector)
     def smooth(self, sensor_r_seq : np.array, f_k : np.array) -> np.array:
-        fb = self.__f # setting a dummy value here...
+        # fb = self.__f # setting a dummy value here...
         # somehow compute fb to be better than f ;-)
         # ...
+        T = self.__tm.get_T()
+        b = np.ones(self.__sm.get_num_of_states())
+        for i in range(len(sensor_r_seq)):
+            O = self.__om.get_o_reading(sensor_r_seq[i])
+            b = T@O@b
+        fb = normalize(f_k*b)
         return fb
